761dca97a96751ff36ff7daa8d548160bb19d94e
Qingchen_Jia <879484952@qq.com>
2024-07-23 13:18:18 +0800

    Adjust the license plate number regular expression

--- src/main/java/SmartBusSystem/service/function/AdminEditBus.java
+++ src/main/java/SmartBusSystem/service/function/AdminEditBus.java
@@ -37,7 +37,7 @@ public class AdminEditBus {
     }
 
     public static boolean checkLicenseNumber(String licenseNum) {
-        String licenseNumberRegex = "[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵青藏川宁琼]([A-Z]·[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]|[DF]·[A-HJ-NP-Z0-9]{5})";
+        String licenseNumberRegex = "[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵青藏川宁琼]([A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]|[DF][A-HJ-NP-Z0-9]{5})";
         return licenseNum.matches(licenseNumberRegex);
     }
 
