0504aec25fb616514b80456025b669a444bcf194
Qingchen_Jia <879484952@qq.com>
2024-07-28 17:19:51 +0800

    Exclude src/test directory

--- src/test/java/Encrypt.java
+++
@@ -1,8 +0,0 @@
-import SmartBusSystem.service.SecurityProtect;
-
-public class Encrypt {
-    public static void main(String[] args) throws Exception {
-        String decrypt = SecurityProtect.decrypt("RmjJCAAXqVAvr9EQsKb5jQ==");
-        System.out.println(decrypt);
-    }
-}
--- src/test/java/LicenseNumberCheck.java
+++
@@ -1,6 +0,0 @@
-public class LicenseNumberCheck {
-    public static void main(String[] args) {
-        String regex = "[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵青藏川宁琼]([A-Z]·[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]|[DF]·[A-HJ-NP-Z0-9]{5})";
-        System.out.println("鄂E·10001".matches(regex));
-    }
-}
