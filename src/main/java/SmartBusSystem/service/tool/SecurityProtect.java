1eeba77abc4e363493ea1649b218286fee860312
Qingchen_Jia <879484952@qq.com>
2024-08-02 18:39:09 +0800

    Add bouncycastle as security provider

--- src/main/java/SmartBusSystem/service/tool/SecurityProtect.java
+++ src/main/java/SmartBusSystem/service/tool/SecurityProtect.java
@@ -1,8 +1,11 @@
 package SmartBusSystem.service.tool;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.Security;
 import java.util.Base64;
 
 public class SecurityProtect {
@@ -11,23 +14,26 @@ public class SecurityProtect {
     private static final String algorithm = "AES/CBC/PKCS5Padding";
 
     static {
-        String keyString = "1234567890123456";  //密钥
-        String ivString = "abcdefghijklmnop";   //向量
+        // 添加BouncyCastle作为安全提供者
+        Security.addProvider(new BouncyCastleProvider());
+
+        String keyString = "1234567890123456";  // 密钥
+        String ivString = "abcdefghijklmnop";   // 向量
         key = new SecretKeySpec(keyString.getBytes(), "AES");
         iv = new IvParameterSpec(ivString.getBytes());
     }
 
-    //AES加密
+    // AES加密
     public static String encrypt(String input) throws Exception {
-        Cipher cipher = Cipher.getInstance(algorithm);
+        Cipher cipher = Cipher.getInstance(algorithm,"BC");
         cipher.init(Cipher.ENCRYPT_MODE, key, iv);
         byte[] cipherText = cipher.doFinal(input.getBytes());
         return Base64.getEncoder().encodeToString(cipherText);
     }
 
-    //AES解密
+    // AES解密
     public static String decrypt(String cipherText) throws Exception {
-        Cipher cipher = Cipher.getInstance(algorithm);
+        Cipher cipher = Cipher.getInstance(algorithm,"BC");
         cipher.init(Cipher.DECRYPT_MODE, key, iv);
         byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
         return new String(plainText);
